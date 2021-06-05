185cd7bf76368e3fff879449a8d3ce07571187c3
Nicolas Giordano <nickgior@gmail.com>
2021-06-05 13:54:29 -0700

    added test cases for homepage and its views and templates

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import HomePageView
 # Create your tests here.
+
+
+class HomePageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.resp = self.client.get(url)
+
+    def test_homepage_status(self):
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.resp, 'home.html')
+
+    def test_homepage_correct_html(self):
+        self.assertContains(self.resp, 'Homepage')
+
+    def test_homepage_wrong_html(self):
+        self.assertNotContains(self.resp, 'I am a test case.')
+
+    def test_homepage_url_resolves_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
