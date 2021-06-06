72387b7c72b9c38df14bb28c5a915adcdce015b4
Nicolas Giordano <nickgior@gmail.com>
2021-06-06 15:29:04 -0700

    created postgres db for stores and items

---
+++ data/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Stores, Items
+# Register your models here.
+
+admin.site.register(Stores)
+admin.site.register(Items)
--- grocery/settings.py
+++ grocery/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 
     'pages.apps.PagesConfig',
+    'data.apps.DataConfig',
 ]
 
 MIDDLEWARE = [
