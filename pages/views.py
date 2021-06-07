7479a6cb0ab0f17e0eb11bcf836a3b5c0ae8ea38
Nicolas Giordano <nickgior@gmail.com>
2021-06-07 16:57:38 -0700

    added ability for user to check individual stores for all items at that store and their prices. Needs formatting

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from typing import List
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Store
 # Create your views here.
 
@@ -8,10 +8,21 @@ from .models import Store
 class HomePageView(ListView):
     model = Store
     template_name = 'home.html'
-    context_object_name = 'store_list'
+    context_object_name = 'stores'
 
 
 class AboutPageView(ListView):
     model = Store
     template_name = 'about.html'
-    context_object_name = 'store_list'
+    context_object_name = 'stores'
+
+
+class StorePageView(DetailView):
+    model = Store
+    template_name = 'store.html'
+    context_object_name = 'store'
+
+    def get_context_data(self, **kwargs):
+        context = super(StorePageView, self).get_context_data(**kwargs)
+        context['stores'] = Store.objects.all()
+        return context
